a304b62dda77e3faa591ca735b6d61808e1d3f4a
Harshith Ashvi <harshith.ashvi@gmail.com>
2024-10-26 21:06:17 +0530

    update code file to create a post card with inputs from modal

--- code.ts
+++ code.ts
@@ -1,3 +1,47 @@
 figma.showUI(__html__);
 
 figma.ui.resize(500, 400);
+
+figma.ui.onmessage = async (pluginMessage) => {
+  await figma.loadAllPagesAsync();
+  await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
+
+  const postComponentSet = figma.root.findOne(
+    (node) => node.type === "COMPONENT_SET" && node.name === "post"
+  ) as ComponentSetNode;
+
+  const imageVariant = {
+    "1": "none",
+    "2": "single",
+    "3": "carousel",
+  };
+
+  const variant = postComponentSet.findOne(
+    (node) =>
+      node.type === "COMPONENT" &&
+      node.name ===
+        `Image=${
+          imageVariant[
+            (pluginMessage.imageVariant || "1") as keyof typeof imageVariant
+          ]
+        }, Dark mode=${pluginMessage.isDarkMode}`
+  ) as ComponentNode;
+
+  const newPost = variant.createInstance();
+
+  const postName = newPost.findOne(
+    (node) => node.name === "displayName" && node.type === "TEXT"
+  ) as TextNode;
+  const postUsername = newPost.findOne(
+    (node) => node.name === "@username" && node.type === "TEXT"
+  ) as TextNode;
+  const postDescription = newPost.findOne(
+    (node) => node.name === "description" && node.type === "TEXT"
+  ) as TextNode;
+
+  postName.characters = pluginMessage.name;
+  postUsername.characters = pluginMessage.username;
+  postDescription.characters = pluginMessage.description;
+
+  figma.closePlugin();
+};
